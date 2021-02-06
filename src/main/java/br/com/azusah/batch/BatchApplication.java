0a2592e0a35b5aef025d7457a413ee6142cc29f3
Daniel Albino <daniel.albino@gmail.com>
2021-02-05 23:18:58 -0300

    changing BatchApplication.

--- src/main/java/br/com/azusah/batch/BatchApplication.java
+++ src/main/java/br/com/azusah/batch/BatchApplication.java
@@ -1,13 +1,18 @@
 package br.com.azusah.batch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BatchApplication {
+public class BatchApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
